0802f412f631741a86c0888d65ca753cd5b1748c
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-01-05 19:16:25 -0300

    style(trailer): finish trailer style

--- src/layouts/Main/Trailer/Trailer.vue
+++ src/layouts/Main/Trailer/Trailer.vue
@@ -1,13 +1,72 @@
 <template>
     <section class="trailer_section">
-
+        <div class="trailer_wrapp">
+            <iframe
+                v-if="trailerKey"
+                :src="`https://www.youtube.com/embed/${trailerKey}`"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen
+            ></iframe>
+        </div>
     </section>
 </template>
 
 <script setup>
+    import { ref, onMounted } from "vue"
+    import { API_KEY, BASE_URL } from "@/config/tmdb"
 
+    const trailerKey = ref(null)
+
+    async function getUpComingMovies() {
+        const response = await fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR`)
+
+        const data = await response.json()
+
+        const firstMovie = data.results[0]
+
+        if (firstMovie?.id) {
+            getTrailer(firstMovie.id)
+        }
+    }
+
+    async function getTrailer(movieId) {
+      const response = await fetch(`${BASE_URL}/movie/${movieId}/videos?api_key=${API_KEY}&language=pt-BR`)
+
+        const data = await response.json()
+
+        const trailer = data.results.find(
+            video => video.type === "Trailer" && video.site === "YouTube"
+        )
+
+        if (trailer) {
+            trailerKey.value = trailer.key
+        }
+    }
+
+    onMounted(() => {
+        getUpComingMovies()
+    })
 </script>
 
 <style scoped>
+    .trailer_section {
+    }
 
+    .trailer_wrapp {
+        padding: 100px 0px;
+
+        position: relative;
+        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        background-color: #EDEDED;
+    }
+
+    .trailer_wrapp > iframe {
+        width: 900px;
+        height: 500px;
+    }
 </style>
